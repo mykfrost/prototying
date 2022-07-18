1a161f8a00ceba53ce8a0149d59ca03dcabbab22
Michael Okello <okellofrost@gmail.com>
2022-07-18 14:44:45 +0300

    new comm

---
+++ index.js
@@ -0,0 +1,12 @@
+function Circle(radius){
+    this.radius = radius;
+}
+
+Circle.prototype.draw= function(){
+ console.log('draw');
+
+}
+
+Circle.prototype.duplicate = function(){
+    console.log('duplicate');
+}
