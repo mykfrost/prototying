a69aecfe5557b0af92e40a02b42917d21ab3702c
Michael Okello <okellofrost@gmail.com>
2022-07-18 15:44:32 +0300

    Making extensions

--- index.js
+++ index.js
@@ -1,26 +1,43 @@
 
-function Shape(){
+function Shape(color){
+    this.color = color;
 
 }
 Shape.prototype.duplicate = function(){
     console.log('duplicate');
    
    }
-  function Circle(radius){
+
+//create a function for creating prototype and resetting constructor
+
+function extend(Child , Parent){
+ Child.prototype = Object.create(Parent.prototype);
+//Resetting constructor so it does not use shape constructor   
+Child.prototype.constructor = Child;
+}
+
+extend(Circle,Shape);
+
+ function Circle(radius,color){
+    Shape.call(this, color);
     this.radius = radius;
 }
 
 //Creat a new cicle object that inherits from shape base object
 
-Circle.prototype = Object.create(Shape.prototype);
-//Resetting constructor so it does not use shape constructor   
-Circle.prototype.constructor = Circle;
+
 Circle.prototype.draw = function(){
        console.log('draw');
    }
+function Square(size){
+    this.size = size;
+}
 
-const s = new Shape();
-const c = new Circle(2);
+extend(Shape,Shape);
+
+const s = new Shape('orange');
+const c = new Circle(2,'red');
+const sq = new Square(100);
 
 console.log(c);
 console.log(s);
